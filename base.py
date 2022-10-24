e74dd130da3e32cab7e568cd5dffa62cef52df77
Flocodev <116581584+Flocodev@users.noreply.github.com>
2022-10-24 18:06:48 +0200

    Add files via upload

---
+++ base.py
@@ -0,0 +1,21 @@
+class config():
+    token = "TON TOKEN"
+    prefix = "+"
+    nstatue = ""
+    yourid = "TON ID"
+    #webhook ping 
+    webhookping = ""
+    #webhook join et leave serv
+    webhookjl = ""
+    #webhook run
+    webhookrun = ""
+    #webhook avatar
+    ppweb = "https://media.discordapp.net/attachments/1033167238796148806/1033167329292451970/spykebn.png"
+    #webhook tools
+    webhooktools = ""
+    #webhook dm all logs 
+    webhookdmall = ""
+    #webhook banall et kick all logs
+    webhoolkball = ""
+    #embed and webhook color
+    color = 0xFF0000
